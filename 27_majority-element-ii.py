8e291c4d8d0decfff9e66cfb4476d97fc7c04dcd
Amar_Asfaw <amarasfaw1077@gmail.com>
2026-02-08 14:13:14 +0300

    updated

--- 27_majority-element-ii.py
+++ 27_majority-element-ii.py
@@ -1,26 +1,10 @@
 from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-       a1,c1 = None,0
-       a2,c2 = None,0
-       ans = []
-       for i in nums:
-        if i==a1:
-            c1+=1
-        elif i == a2:
-            c2+=1
-        elif c1 == 0:
-            a1 = i
-            c1=1
-        elif c2 == 0:
-            a2 = i
-            c2=1
-        else:
-            c1-=1
-            c2-=1
-
-       for i in [a1,a2]:
-        if i is not None and nums.count(i)>len(nums)/3:
-            ans.append(i)
-       return list(set(ans))
-
+        x = len(nums)//3
+        ans = []
+        y = Counter(nums)
+        for key,val in y.items():
+            if val>x:
+                ans.append(key)
+        return ans
