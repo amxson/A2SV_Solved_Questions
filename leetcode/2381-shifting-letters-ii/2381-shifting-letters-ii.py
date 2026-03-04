1dbd8f6d008c51a21fbd5e177e1dc034ebd6dc41
Amar Asfaw <91834366+amxson@users.noreply.github.com>
2026-03-04 16:19:04 +0300

    Add solution for Shifting Letters II

---
+++ leetcode/2381-shifting-letters-ii/2381-shifting-letters-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        n = len(s)
+        shift = [0]*n
+        for l,r,k in shifts:
+            if k == 0:
+                shift[l]-=1
+                if r+1<n:
+                    shift[r+1]+= 1
+            else:
+                shift[l]+= 1
+                if r+1<n:
+                    shift[r+1]-=1
+        c = 0
+        for i in range(len(shift)):
+            c+=shift[i]
+            shift[i]=c
+        ans = []
+        for i in range(n):
+            num = ord(s[i]) - ord('a') 
+            new =  (num + shift[i]) % 26
+            ans.append(chr(new + ord('a')))
+        return(''.join(ans))
+
+
+
+
+
+
+        
