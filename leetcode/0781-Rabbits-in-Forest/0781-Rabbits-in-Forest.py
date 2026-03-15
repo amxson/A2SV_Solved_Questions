fb4d5c0818c3faeda73d0d3be1f3b8306a30d45a
Amar Asfaw <91834366+amxson@users.noreply.github.com>
2026-03-15 15:45:36 +0300

    Add solution for Rabbits in Forest

---
+++ leetcode/0781-Rabbits-in-Forest/0781-Rabbits-in-Forest.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numRabbits(self, answers: List[int]) -> int:
+        zero = 0
+        sett = defaultdict(int)
+        for i in answers:
+            sett[i]+=1
+
+        t= 0
+        for i,j in sett.items():
+                if i == 0:
+                    t+=sett[i]
+                else:
+                    x = sett[i]
+                    while x>i+1:
+                        t+= i+1
+                        x-=i+1
+                    t+=i+1
+        return t
