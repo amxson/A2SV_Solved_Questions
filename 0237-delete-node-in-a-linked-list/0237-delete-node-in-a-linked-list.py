7f157c3deabd131ac203038cbe8fd34c8b573c3a
Amar Asfaw <91834366+amxson@users.noreply.github.com>
2026-03-15 21:39:12 +0300

    Time: 45 ms (72.87%), Space: 19.4 MB (88.16%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
@@ -6,11 +6,8 @@
 
 class Solution:
     def deleteNode(self, node):
-        while node.next.next:
-            node.val = node.next.val
-            node = node.next
         node.val = node.next.val
-        node.next=None
+        node.next=node.next.next
 
 
 
