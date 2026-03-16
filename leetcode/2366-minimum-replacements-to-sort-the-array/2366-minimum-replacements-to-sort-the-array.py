234dade41bec10cfe76ee11144fe3eafc8989210
Amar Asfaw <91834366+amxson@users.noreply.github.com>
2026-03-16 21:03:47 +0300

    Add solution for Minimum Replacements to Sort the Array

---
+++ leetcode/2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.py
@@ -0,0 +1,15 @@
+import math
+class Solution:
+    def minimumReplacement(self, nums: List[int]) -> int:
+        ans  = 0
+        l = nums[-1]
+        for i in range(len(nums)-2,-1,-1):
+            if l<nums[i]:   
+                p = math.ceil(nums[i]/l)
+                ans +=p-1
+                l = nums[i]//p
+                continue
+            l = nums[i]
+        return ans
+
+        
